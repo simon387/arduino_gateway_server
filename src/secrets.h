5330d477b87512485a9cd53e02cbc2aa6854e67a
Simone Celia <simone.celia@eng.it>
2025-03-13 20:00:51 +0100

    first version

---
+++ src/secrets.h
@@ -0,0 +1,8 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// Configura qui le tue credenziali WiFi
+const char* ssid = "";      // Sostituisci con il nome della tua rete WiFi
+const char* password = "";  // Sostituisci con la password della tua rete WiFi
+
+#endif
